8cfbe7e58ff44a5d70c1d8da86de4569cc501268
kashan ghori <kashanghoriii@gmail.com>
2024-11-23 02:39:55 +0500

    added chad cn ui and lottieJS

---
+++ src/app/components/lottie.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import React from 'react'
+
+const lottie = () => {
+  return (
+    <DotLottieReact
+    src="/chicken.lottie"
+    loop
+    autoplay
+  />
+  )
+}
+
+export default lottie
