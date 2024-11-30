9d7260c9faf4486360327899ad04c1f4c6455193
noumandatu <noumankhan619.915@gmail.com>
2024-11-30 19:16:01 +0500

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./sections/hero";
 import Image from "next/image";
 import Navbar from "./global-components/navbar";
@@ -11,10 +11,22 @@ import { ToastContainer } from "react-toastify";
 import StoryLoader from "./global-components/storyLoader";
 import StoryResponse from "./sections/storyresponse";
 import { useAppStore } from "@/lib/store";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 const page = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { story, isLoading } = useAppStore();
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Animation duration in ms
+      easing: "ease-out-back", // Easing function
+    });
+    return () => {
+      AOS.refresh();
+    };
+  }, []);
 
   return (
     <div>
@@ -23,15 +35,15 @@ const page = () => {
         <Navbar />
         <div className="flex justify-between items-center">
           <div>
-            <Image src={rightCloud} alt="Right Cloud" width={308} height={107} className="justify-self-end" />
-            <Image src={rightTress} alt="Right Tree" width={359} height={407} className="justify-self-end" />
+            <Image src={rightCloud} alt="Right Cloud" data-aos="fade-right" width={308} height={107} className="justify-self-end" />
+            <Image src={rightTress} alt="Right Tree" data-aos="fade-up-right" width={359} height={407} className="justify-self-end" />
           </div>
-          <div>
+          <div data-aos="zoom-in">
             <Hero />
           </div>
           <div>
-            <Image src={leftCloud} alt="Left Cloud" width={306} height={125} className="justify-self-start" />
-            <Image src={leftTress} alt="Left Tree" width={359} height={407} className="justify-self-start" />
+            <Image src={leftCloud} alt="Left Cloud" data-aos="fade-left" width={306} height={125} className="justify-self-start" />
+            <Image src={leftTress} alt="Left Tree" data-aos="fade-up-left" width={359} height={407} className="justify-self-start" />
           </div>
         </div>
       </div>
